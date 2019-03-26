dc8f957c4232a8b045fcad8d673ecf436df54c24
Wietse Vandeput <wietse.vandeput@ucll.student.be>
2019-03-26 12:02:49 +0100

    still not working thanks to the bean thing but dependencies fixed

--- src/main/java/be/ucll/umami/Application.java
+++ src/main/java/be/ucll/umami/Application.java
@@ -9,7 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.annotation.Order;
 
 import java.util.Arrays;
